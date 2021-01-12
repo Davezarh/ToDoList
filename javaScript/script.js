7da60dc5d3dde6bda317dc4763135f31d9bc2ff7
Dawid <dharendarz2@gmail.com>
2021-01-12 23:04:58 +0100

    Adding new task, to array.  You cannot add task to array, which is empty.

--- javaScript/script.js
+++ javaScript/script.js
@@ -2,7 +2,6 @@ let tasks = [];
 
 const form = document.querySelector(".form");
 const todoButton = document.querySelector(".todo-button");
-const todoInput = document.querySelector(".input-todo");
 const todoList = document.querySelector(".todo-list")
 
 const liTodo = () =>{
@@ -16,14 +15,28 @@ const liTodo = () =>{
     const trashButton = document.createElement("button")
     trashButton.classList.add("trashButton")
     todoDiv.appendChild(trashButton)
-
     const doneButton = document.createElement("button")
     doneButton.classList.add("doneButton")
     todoDiv.appendChild(doneButton)
 
     todoList.appendChild(todoDiv)
+    newTodo.innerText = (`Testing`);    
+}
+const addNewTask = (newContent) =>{
+        tasks = [
+            ...tasks,
+            {content: newContent}
+        ]
 }
 const onFormSubmit = form.addEventListener("submit",(event)=>{
     event.preventDefault();
-    liTodo();
+    const newContent = document.querySelector(".input-todo").value.trim();
+    if(newContent === "")
+    return;
+    else{
+        form.reset();
+    }
+    addNewTask(newContent)
+    console.log(newContent,tasks)
+    liTodo()  
 })
