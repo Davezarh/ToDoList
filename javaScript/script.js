f7ab4b39865666cef28f5b54bb21a030b0c8da3f
Dawid <dharendarz2@gmail.com>
2021-01-13 23:22:13 +0100

    Tasks, show at li list. Added some classes for icons from font Awesome

--- javaScript/script.js
+++ javaScript/script.js
@@ -4,39 +4,40 @@ const form = document.querySelector(".form");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list")
 
-const liTodo = () =>{
-    const todoDiv = document.createElement("div") 
-    todoDiv.classList.add("todo");
-
-    const newTodo = document.createElement("li");
-    newTodo.classList.add("todo-item")
-    todoDiv.appendChild(newTodo)
-
-    const trashButton = document.createElement("button")
-    trashButton.classList.add("trashButton")
-    todoDiv.appendChild(trashButton)
-    const doneButton = document.createElement("button")
-    doneButton.classList.add("doneButton")
-    todoDiv.appendChild(doneButton)
-
-    todoList.appendChild(todoDiv)
-    newTodo.innerText = (`Testing`);    
-}
 const addNewTask = (newContent) =>{
-        tasks = [
-            ...tasks,
-            {content: newContent}
-        ]
+    tasks.push(newContent)
 }
 const onFormSubmit = form.addEventListener("submit",(event)=>{
     event.preventDefault();
     const newContent = document.querySelector(".input-todo").value.trim();
+
     if(newContent === "")
     return;
     else{
         form.reset();
     }
-    addNewTask(newContent)
-    console.log(newContent,tasks)
+    addNewTask(newContent) 
     liTodo()  
 })
+
+const liTodo = () =>{
+    const todoDiv = document.createElement("div") 
+    todoDiv.classList.add("todo");
+
+    const newTodo = document.createElement("li")
+    newTodo.classList.add("todo-item")
+    todoDiv.appendChild(newTodo)
+
+    const doneButton = document.createElement("button")
+    doneButton.innerHTML = "<i class='fas fa-check'></i>";
+    doneButton.classList.add("complete-btn")
+    todoDiv.appendChild(doneButton)
+
+    const trashButton = document.createElement("button")
+    trashButton.innerHTML = "<i class='fas fa-trash'></i>";
+    trashButton.classList.add("complete-btn")
+    todoDiv.appendChild(trashButton)
+
+    todoList.appendChild(todoDiv)
+    newTodo.innerHTML = tasks
+}
