22f745efc39f32d483e005fe6ec7ade725ea6eb0
Dawid <dharendarz2@gmail.com>
2021-01-18 10:02:07 +0100

    Added two buttons with functions like show Tasks and mark all done.Focus is available when u click+

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,4 +1,6 @@
 let tasks = [];
+let hideTasksDone = false;
+
     const addNewTask = (newContent) =>{
         tasks = 
         [
@@ -7,6 +9,7 @@ let tasks = [];
         ]
         render()
     }
+
     const removeTask = (index) =>{
         tasks =
         [
@@ -15,6 +18,7 @@ let tasks = [];
         ]
         render()
     }
+
     const toggleTaskDone = (index) =>{
        tasks = [
             ...tasks.slice(0,index), 
@@ -24,11 +28,24 @@ let tasks = [];
             ...tasks.slice(index + 1), 
         ]
         render()
-        
+     }
+
+    const markAllDone = () =>{
+       tasks = tasks.map((task)=> ({
+            ...task,
+            done:true
+          }))   
+        render();
     }
+
+    const hideDoneTasks = () =>{
+        hideTasksDone =! hideTasksDone;   
+        render();
+    }
+
     const renderTasks = () => {
         const taskHtml = task =>`
-         <li class="todo__Item ${task.done ? "todo__Item--Done" : ""}">
+         <li class="todo__Item ${task.done && hideTasksDone ? "todo__Item--Done": ""}">
             <span class="todo__Content ${task.done ? "todo__Content--done" : ""}">
                 ${task.content}
             </span>
@@ -45,7 +62,17 @@ let tasks = [];
     const taskElement = document.querySelector(".todo__List");
     taskElement.innerHTML = tasks.map(taskHtml).join("");
     }
+
     const renderButtons = () =>{
+        const buttons = document.querySelector(".todo__Buttons")
+        if(!tasks.length){ buttons.innerHTML = ""; return; }
+
+        buttons.innerHTML=`
+        <button class ="markAllDone" ${tasks.every(({done}) => done ) ?"disabled" :""}>Ukończ wszystkie</button>
+        <button class ="hideDoneTasks"> ${hideTasksDone ? "Pokaż" : "Ukryj"} ukończone</button>
+        `
+    }
+    const bindEvents = () =>{
         const removeButton = document.querySelectorAll(".todo__TrashButton")
         removeButton.forEach((removedButton, index) => {
          removedButton.addEventListener("click", () => {
@@ -58,34 +85,50 @@ let tasks = [];
                 toggleTaskDone(index)
             })
         })
+
+    const markDoneButton = document.querySelector(".markAllDone")
+    if(markDoneButton){ markDoneButton.addEventListener("click",markAllDone) }
+
+    const hideDoneButton = document.querySelector(".hideDoneTasks")
+    if(hideDoneButton)
+    { hideDoneButton.addEventListener("click",hideDoneTasks) }
     
     }
-    const render = () =>{
-        renderTasks()
-        renderButtons()
-    }
+   
     const formReset = () =>{
         document.querySelector(".form").reset()
+    }
+
+    const focusInput = () => {
+        const input = document.querySelector(".todo__Input")
+        input.focus();
+    }
+    const resetInput = () =>{
+        const resetButton = document.querySelector(".todo__Button")
+        resetButton.addEventListener("click",focusInput)
+    }
+
+    const render = () => {
+        renderTasks()
+        renderButtons()
+        bindEvents()
+        resetInput()
     }
     const onFormSubmit = (event) => {
         event.preventDefault();
         const newContent = document.querySelector(".todo__Input").value.trim();
        
-         if(newContent === ""){
-            return; 
-         }
-            else{
-                formReset()
-             } 
+         if(newContent === ""){ return; }
+            else{ formReset() } 
+
         addNewTask(newContent)  
-        localStorage.setItem("tasks",JSON.stringify(tasks)) 
         render()
     }
+
     const init = () => {
         render()
         const form = document.querySelector(".form");
         form.addEventListener("submit",onFormSubmit)  
     }
 
-    
 init();
